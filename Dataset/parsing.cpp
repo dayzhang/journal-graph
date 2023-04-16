2ace025f2e739413429af218d067819183126399
Daniel Zhang <dze61624@gmail.com>
2023-04-16 19:58:50 +0000

    json library

---
+++ Dataset/parsing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+using namespace std;
+using json = nlohmann::json;
+int main()
+{
+std::ifstream f(“data.json”);
+json data = json::parse(f);
+// Access the values existing in JSON data
+string name = data.value(“name”, “not found”);
+string grade = data.value(“grade”, “not found”);
+// Access a value that does not exist in the JSON data
+string email = data.value(“email”, “not found”);
+// Print the values
+cout << “Name: “ << name << endl;
+cout << “Grade: “ << grade << endl;
+cout << “Email: “ << email << endl;
+return 0;}
