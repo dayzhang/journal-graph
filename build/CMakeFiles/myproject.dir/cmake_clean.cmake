0d343f044fa6d786cf0e8593b40ecae7be2ee83a
19713 <kevinchensunshine@gmail.com>
2023-04-17 19:33:33 +0000

    MakeFile added, currently configured for main. cd into build, then run make main && ./main

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/main.cpp.o"
+  "CMakeFiles/myproject.dir/main.cpp.o.d"
+  "myproject"
+  "myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#define exit_failure 0
+#define exit_success 1
+
+int main() {
+    std::cout << "Hello World";
+    return exit_success;
+}
