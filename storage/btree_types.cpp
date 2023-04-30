2daf407fcf1a78fa5faf301018f7e9f65365deac
yianzhang14 <ianz2@illinois.edu>
2023-04-30 23:38:46 +0000

    templatized btree db

---
+++ storage/btree_types.cpp
@@ -0,0 +1,118 @@
+#include <array>
+#include <cstring>
+#include <string>
+#include <vector>
+
+namespace author {
+    struct Entry {
+        std::array<char, 40> name;
+        std::array<char, 100> organization;
+
+        Entry(const std::string& author_name, const std::string& author_org) {
+            if (author_name.size() >= 40) {
+                strcpy(name.data(), author_name.substr(0, 39).c_str());
+            } else {
+                strcpy(name.data(), author_name.c_str());
+            }
+
+            if (author_org.size() >= 100) {
+                strcpy(organization.data(), author_org.substr(0, 99).c_str());
+            } else {
+                strcpy(organization.data(), author_org.c_str());
+            }
+        }
+
+        Entry() {}
+    };
+
+    void deserialize_value(char* source, Entry* dest) {
+        memcpy(dest->name.data(), source, 40);
+        memcpy(dest->organization.data(), source + 40, 100);
+    }
+
+    void serialize_value(Entry* source, char* dest) {
+        memcpy(dest, source->name.data(), 40);
+        memcpy(dest + 40, source->organization.data(), 100);
+    }
+
+    unsigned int size = 140;
+}
+
+namespace paper {
+    struct Entry {
+        std::array<char, 60> title;
+        std::array<char, 60> keywords;
+        std::array<char, 40> venue;
+        std::array<long, 10> authors;
+
+        unsigned int n_citations;
+        unsigned int pub_year;
+
+        Entry(std::string& paper_title, std::string& paper_keywords, std::string& paper_venue, unsigned int paper_citations, unsigned int paper_year, std::vector<long> paper_authors) {
+            if (title.size() >= 60) {
+                strcpy(title.data(), paper_title.substr(0, 59).c_str());
+            } else {
+                strcpy(title.data(), paper_title.c_str());
+            }
+
+            if (paper_keywords.size() >= 60) {
+                strcpy(title.data(), paper_keywords.substr(0, 59).c_str());
+            } else {
+                strcpy(title.data(), paper_keywords.c_str());
+            }
+
+            if (paper_venue.size() >= 40) {
+                strcpy(title.data(), paper_venue.substr(0, 39).c_str());
+            } else {
+                strcpy(title.data(), paper_venue.c_str());
+            }
+
+            n_citations = paper_citations;
+            pub_year = paper_year;
+
+            for (unsigned int i = 0; i < 10; ++i) {
+                authors[i] = paper_authors[i];
+            }
+        }
+
+        Entry() {}
+    };
+
+    void deserialize_value(char* source, Entry* dest) {
+        memcpy(dest->title.data(), source, 60);
+        memcpy(dest->keywords.data(), source + 60, 60);
+        memcpy(dest->venue.data(), source + 120, 40);
+        memcpy(dest->authors.data(), source + 160, 80);
+        memcpy(&(dest->n_citations), source + 240, 4);
+        memcpy(&(dest->pub_year), source + 244, 4);
+    }
+
+    void serialize_value(Entry* source, char* dest) {
+        memcpy(dest, source->title.data(), 60);
+        memcpy(dest + 60, source->keywords.data(), 60);
+        memcpy(dest + 120, source->venue.data(), 40);
+        memcpy(dest + 160, source->authors.data(), 80);
+        memcpy(dest + 240, &(source->n_citations), 4);
+        memcpy(dest + 244, &(source->pub_year), 4);
+    }
+
+    unsigned int size = 248;
+}
+
+namespace test {
+    struct Entry {
+        int x;
+        Entry(int x): x(x) {}
+        Entry() {}
+    };
+
+    void deserialize_value(char* source, Entry* dest) {
+        memcpy(&(dest->x), source, 4);
+    }
+
+    void serialize_value(Entry* source, char* dest) {
+        memcpy(dest, &(source->x), 4);
+    }
+
+    unsigned int size = 4;
+}
